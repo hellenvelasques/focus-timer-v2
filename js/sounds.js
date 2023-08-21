493a44d398bcb17243f4cbbc6345abacd8281389
hellenvelasques <hgv92jc@gmail.com>
2023-08-21 08:40:16 -0300

    fix: ajuste no audios.

--- js/sounds.js
+++ js/sounds.js
@@ -1,10 +1,10 @@
 export default function() {
-  const buttonPressAudio = new Audio ('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/audios_button-press.wav');
-  const kitchenTimer = new Audio ('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/audios_kichen-timer.mp3');
-  const songForest = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Floresta.wav');
-  const songRain = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Chuva.wav');
-  const songCoffeeShop = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Cafeteria.wav');
-  const songFirePlace = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Lareira.wav');
+  const buttonPressAudio = new Audio ('../sounds/audios_button-press.mp3');
+  const kitchenTimer = new Audio ('../sounds/audios_kichen-timer.mp3');
+  const forestAudio = new Audio('../sounds/Floresta.mp3');
+  const rainAudio = new Audio('../sounds/Chuva.mp3 ');
+  const coffeeAudio = new Audio('../sounds/Cafeteria.mp3 ');
+  const fireplaceAudio = new Audio('../sounds/Lareira.mp3');
 
   forestAudio.loop = true
   rainAudio.loop = true
