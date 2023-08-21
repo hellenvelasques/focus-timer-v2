669cd2e6d972460405f76c3ead45530fc2a0b8c1
hellenvelasques <hgv92jc@gmail.com>
2023-08-21 09:43:32 -0300

    fix: links dos audios

--- js/sounds.js
+++ js/sounds.js
@@ -1,10 +1,18 @@
 export default function() {
-  const buttonPressAudio = new Audio ('../sounds/audios_button-press.mp3');
-  const kitchenTimer = new Audio ('../sounds/audios_kichen-timer.mp3');
-  const forestAudio = new Audio('../sounds/Floresta.mp3');
-  const rainAudio = new Audio('../sounds/Chuva.mp3 ');
-  const coffeeAudio = new Audio('../sounds/Cafeteria.mp3 ');
-  const fireplaceAudio = new Audio('../sounds/Lareira.mp3');
+  const buttonPressAudio = new Audio ('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/audios_button-press.mp3');
+  const kitchenTimer = new Audio ('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/audios_kichen-timer.mp3');
+  const forestAudio = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Floresta.mp3');
+  const rainAudio = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Chuva.mp3');
+  const coffeeAudio = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Cafeteria.mp3');
+  const fireplaceAudio = new Audio('https://github.com/hellenvelasques/focus-timer-v2/blob/main/sounds/Lareira.mp3');
+  
+  //local
+  // const buttonPressAudio = new Audio ('../sounds/audios_button-press.mp3');
+  // const kitchenTimer = new Audio ('../sounds/audios_kichen-timer.mp3');
+  // const forestAudio = new Audio('../sounds/Floresta.mp3');
+  // const rainAudio = new Audio('../sounds/Chuva.mp3 ');
+  // const coffeeAudio = new Audio('../sounds/Cafeteria.mp3 ');
+  // const fireplaceAudio = new Audio('../sounds/Lareira.mp3');
 
   forestAudio.loop = true
   rainAudio.loop = true
